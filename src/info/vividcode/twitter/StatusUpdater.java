5b375af92296783f602c31ce86e8cadf28a33c9d
nobuoka <nobuoka@r-definition.com>
2011-07-21 03:53:01 +0900

    refactoring on StatusUpdater

--- src/info/vividcode/twitter/StatusUpdater.java
+++ src/info/vividcode/twitter/StatusUpdater.java
@@ -3,18 +3,20 @@ package info.vividcode.twitter;
 import info.vividcode.app.twitvc.Config;
 import info.vividcode.twitter.CredentialManager.Credential;
 import info.vividcode.util.oauth.OAuthRequestHelper;
-import info.vividcode.util.oauth.OAuthRequestHelper.Param;
 
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.io.Reader;
 import java.util.Date;
 
 import org.apache.http.HttpEntity;
 import org.apache.http.HttpResponse;
 import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
 import org.apache.http.client.methods.HttpPost;
+import org.apache.http.client.methods.HttpUriRequest;
 import org.apache.http.entity.StringEntity;
 import org.apache.http.impl.client.DefaultHttpClient;
 import org.apache.http.params.BasicHttpParams;
@@ -47,101 +49,15 @@ public class StatusUpdater {
 	
 	public static void update( String text, Credential c, String idInReplyTo ) throws Exception {
 		
-		String accToken  = c.getToken();
-		String accSecret = c.getSecret();
-		String conKey    = Config.getConsumerKey();
-		String conSecret = Config.getConsumerSecret();
-		
-		// cf. http://d.hatena.ne.jp/tototoshi/20100117/1263711661 (OAuth)
-		Exception exception = null;
-		// TODO
-		//HttpURLConnection conn = null;
-		HttpClient httpclient = null;
-		InputStream is = null;
-		try {
-			String urlStr = "http://api.twitter.com/1/statuses/update.json";
-			String method = "POST";
-			String secretsStr = conSecret + "&" + accSecret;
-			
-			OAuthRequestHelper.ParamList aParamList = new OAuthRequestHelper.ParamList( 
-					new String[][]{
-							{ "oauth_consumer_key", conKey },
-							{ "oauth_token", accToken },
-							{ "oauth_nonce", OAuthRequestHelper.getNonceString() },
-							{ "oauth_signature_method", "HMAC-SHA1" },
-							{ "oauth_timestamp", Long.toString( new Date().getTime() / 1000 ) },
-							{ "oauth_version", "1.0" },
-						} );
-			OAuthRequestHelper.ParamList bParamList = new OAuthRequestHelper.ParamList( 
-					new String[][]{
-							{ "status", replaceXmlEntityReference( text ) },
-						} );
-			if( idInReplyTo != null ) {
-				bParamList.add( new Param( "in_reply_to_status_id", idInReplyTo ) );
-			}
-			OAuthRequestHelper helper = new OAuthRequestHelper( 
-					urlStr, method, secretsStr, aParamList, null, bParamList );
-			
-			// TODO
-			// タイムアウトの指定
-			HttpParams httpParams = new BasicHttpParams();
-			HttpConnectionParams.setConnectionTimeout( httpParams, 100000 );
-			HttpConnectionParams.setSoTimeout( httpParams, 10000 );
-			
-			httpclient = new DefaultHttpClient( httpParams );
-			HttpPost req = new HttpPost( helper.getUrlStringIncludeQueryParams() );
-			req.addHeader( "Authorization", helper.getAuthorizationHeaderString( "" ) );
-			req.addHeader( "Content-Type", "application/x-www-form-urlencoded" );
-			// debug
-			System.out.println( "[DEBUG] " + helper.getRequestBodyString() );
-			req.setEntity( new StringEntity( helper.getRequestBodyString(), "US-ASCII" ) );
-			HttpResponse res = httpclient.execute( req );
-			int statusCode = res.getStatusLine().getStatusCode();
-			if( statusCode != 200 ) {
-				// 400 番台のエラーの場合はエラーメッセージを取得する
-				String responseBody = null;
-				if( true ) {//if( 400 <= statusCode && statusCode < 500 ) {
-					BufferedReader br = null;
-					StringBuilder  sb = new StringBuilder();
-					try {
-						br = new BufferedReader( 
-								new InputStreamReader( res.getEntity().getContent() ) );
-						String s = null;
-						while( ( s = br.readLine() ) != null ) {
-							sb.append( s );
-						}
-					} finally {
-						br.close();
-					}
-					responseBody = sb.toString();
-				}
-				throw new RuntimeException( "Twitter API サーバーからエラーが返ってきました\n" + 
-						"レスポンスコード : " + statusCode + " " + 
-						res.getStatusLine().getReasonPhrase() +
-						( responseBody == null ? "" : "\nレスポンスボディ : " + responseBody ) );
-			} else {
-				System.out.println( res.getStatusLine().getStatusCode() );
-				System.out.println( res.getEntity() );
-			}
-			HttpEntity ent = res.getEntity();
-			is = ent.getContent();
-			System.out.println( is.getClass() );
-			
-		} catch( Exception ex ) {
-			// TODO Auto-generated catch block
-			//e.printStackTrace();
-			throw new Exception( ex );
-		} finally {
-			try {
-				// 明示的に閉じる必要はあるのか?
-				if( is != null ) is.close();
-			} catch( IOException ex ) {
-				ex.printStackTrace();
-			}
-			// TODO
-			//if( conn != null ) conn.disconnect();
-            if( httpclient != null ) httpclient.getConnectionManager().shutdown();
+		String[][] bparams = new String[( idInReplyTo != null ? 2 : 1 )][];
+		bparams[0] = new String[]{ "status", replaceXmlEntityReference( text ) };
+		if( idInReplyTo != null ) {
+			bparams[1] = new String[]{ "in_reply_to_status_id", idInReplyTo };
 		}
+		sendRequestToTwitterApiServer( 
+				"http://api.twitter.com/1/statuses/update.json", 
+				"POST", null, bparams );
+		
 	}
 	
 	public static void favorite( String idStr ) throws Exception {
@@ -150,96 +66,10 @@ public class StatusUpdater {
 			throw new NullPointerException( "引数 idStr が null です..." );
 		}
 		
-		// TODO null チェック
-		Credential c = mAccessCredentials;
-		String accToken  = c.getToken();
-		String accSecret = c.getSecret();
-		String conKey    = Config.getConsumerKey();
-		String conSecret = Config.getConsumerSecret();
+		sendRequestToTwitterApiServer( 
+				"http://api.twitter.com/1/favorites/create/" + idStr + ".json", 
+				"POST", null, null );
 		
-		// cf. http://d.hatena.ne.jp/tototoshi/20100117/1263711661 (OAuth)
-		Exception exception = null;
-		// TODO
-		//HttpURLConnection conn = null;
-		HttpClient httpclient = null;
-		InputStream is = null;
-		try {
-			String urlStr = "http://api.twitter.com/1/favorites/create/" + idStr + ".json";
-			String method = "POST";
-			String secretsStr = conSecret + "&" + accSecret;
-			
-			OAuthRequestHelper.ParamList aParamList = new OAuthRequestHelper.ParamList( 
-					new String[][]{
-							{ "oauth_consumer_key", conKey },
-							{ "oauth_token", accToken },
-							{ "oauth_nonce", OAuthRequestHelper.getNonceString() },
-							{ "oauth_signature_method", "HMAC-SHA1" },
-							{ "oauth_timestamp", Long.toString( new Date().getTime() / 1000 ) },
-							{ "oauth_version", "1.0" },
-						} );
-			OAuthRequestHelper helper = new OAuthRequestHelper( 
-					urlStr, method, secretsStr, aParamList, null, null );
-			
-			// TODO
-			// タイムアウトの指定
-			HttpParams httpParams = new BasicHttpParams();
-			HttpConnectionParams.setConnectionTimeout( httpParams, 100000 );
-			HttpConnectionParams.setSoTimeout( httpParams, 10000 );
-			
-			httpclient = new DefaultHttpClient( httpParams );
-			HttpPost req = new HttpPost( helper.getUrlStringIncludeQueryParams() );
-			req.addHeader( "Authorization", helper.getAuthorizationHeaderString( "" ) );
-			req.addHeader( "Content-Type", "application/x-www-form-urlencoded" );
-			// debug
-			System.out.println( "[DEBUG] " + helper.getRequestBodyString() );
-			req.setEntity( new StringEntity( helper.getRequestBodyString(), "US-ASCII" ) );
-			HttpResponse res = httpclient.execute( req );
-			int statusCode = res.getStatusLine().getStatusCode();
-			if( statusCode != 200 ) {
-				// 400 番台のエラーの場合はエラーメッセージを取得する
-				String responseBody = null;
-				if( true ) {//if( 400 <= statusCode && statusCode < 500 ) {
-					BufferedReader br = null;
-					StringBuilder  sb = new StringBuilder();
-					try {
-						br = new BufferedReader( 
-								new InputStreamReader( res.getEntity().getContent() ) );
-						String s = null;
-						while( ( s = br.readLine() ) != null ) {
-							sb.append( s );
-						}
-					} finally {
-						br.close();
-					}
-					responseBody = sb.toString();
-				}
-				throw new RuntimeException( "Twitter API サーバーからエラーが返ってきました\n" + 
-						"レスポンスコード : " + statusCode + " " + 
-						res.getStatusLine().getReasonPhrase() +
-						( responseBody == null ? "" : "\nレスポンスボディ : " + responseBody ) );
-			} else {
-				System.out.println( res.getStatusLine().getStatusCode() );
-				System.out.println( res.getEntity() );
-			}
-			HttpEntity ent = res.getEntity();
-			is = ent.getContent();
-			System.out.println( is.getClass() );
-			
-		} catch( Exception ex ) {
-			// TODO Auto-generated catch block
-			//e.printStackTrace();
-			throw new Exception( ex );
-		} finally {
-			try {
-				// 明示的に閉じる必要はあるのか?
-				if( is != null ) is.close();
-			} catch( IOException ex ) {
-				ex.printStackTrace();
-			}
-			// TODO
-			//if( conn != null ) conn.disconnect();
-            if( httpclient != null ) httpclient.getConnectionManager().shutdown();
-		}
 	}
 	
 	public static void retweet( String idStr ) throws Exception {
@@ -247,8 +77,17 @@ public class StatusUpdater {
 		if( idStr == null ) {
 			throw new NullPointerException( "引数 idStr が null です..." );
 		}
+		sendRequestToTwitterApiServer( 
+				"http://api.twitter.com/1/statuses/retweet/" + idStr + ".json", 
+				"POST", null, null );
 		
-		// TODO null チェック
+	}
+	
+	public static String sendRequestToTwitterApiServer( String urlStr, String method, String[][] queryParams, String[][] bodyParams ) throws Exception {
+		
+		// TODO 引数チェック
+		
+		// OAuth 準備
 		Credential c = mAccessCredentials;
 		String accToken  = c.getToken();
 		String accSecret = c.getSecret();
@@ -257,13 +96,12 @@ public class StatusUpdater {
 		
 		// cf. http://d.hatena.ne.jp/tototoshi/20100117/1263711661 (OAuth)
 		Exception exception = null;
+		String responseBody = null;
 		// TODO
 		//HttpURLConnection conn = null;
 		HttpClient httpclient = null;
 		InputStream is = null;
 		try {
-			String urlStr = "http://api.twitter.com/1/statuses/retweet/" + idStr + ".json";
-			String method = "POST";
 			String secretsStr = conSecret + "&" + accSecret;
 			
 			OAuthRequestHelper.ParamList aParamList = new OAuthRequestHelper.ParamList( 
@@ -275,8 +113,16 @@ public class StatusUpdater {
 							{ "oauth_timestamp", Long.toString( new Date().getTime() / 1000 ) },
 							{ "oauth_version", "1.0" },
 						} );
+			OAuthRequestHelper.ParamList qParamList = null;
+			OAuthRequestHelper.ParamList bParamList = null;
+			if( queryParams != null ) {
+				qParamList = new OAuthRequestHelper.ParamList( queryParams );
+			}
+			if( bodyParams != null ) {
+				bParamList = new OAuthRequestHelper.ParamList( bodyParams );
+			}
 			OAuthRequestHelper helper = new OAuthRequestHelper( 
-					urlStr, method, secretsStr, aParamList, null, null );
+					urlStr, method, secretsStr, aParamList, qParamList, bParamList );
 			
 			// TODO
 			// タイムアウトの指定
@@ -285,17 +131,23 @@ public class StatusUpdater {
 			HttpConnectionParams.setSoTimeout( httpParams, 10000 );
 			
 			httpclient = new DefaultHttpClient( httpParams );
-			HttpPost req = new HttpPost( helper.getUrlStringIncludeQueryParams() );
+			HttpUriRequest req = null;
+			if( method.equals( "POST" ) ) {
+				req = new HttpPost( helper.getUrlStringIncludeQueryParams() );
+				((HttpPost) req).setEntity( 
+						new StringEntity( helper.getRequestBodyString(), "US-ASCII" ) );
+			} else if( method.equals( "GET" ) ) {
+				req = new HttpGet( helper.getUrlStringIncludeQueryParams() );
+			} else {
+				throw new RuntimeException( "サポートされていないリクエストメソッド (" + method + ")" );
+			}
 			req.addHeader( "Authorization", helper.getAuthorizationHeaderString( "" ) );
 			req.addHeader( "Content-Type", "application/x-www-form-urlencoded" );
-			// debug
-			System.out.println( "[DEBUG] " + helper.getRequestBodyString() );
-			req.setEntity( new StringEntity( helper.getRequestBodyString(), "US-ASCII" ) );
 			HttpResponse res = httpclient.execute( req );
 			int statusCode = res.getStatusLine().getStatusCode();
 			if( statusCode != 200 ) {
 				// 400 番台のエラーの場合はエラーメッセージを取得する
-				String responseBody = null;
+				//String responseBody = null;
 				if( true ) {//if( 400 <= statusCode && statusCode < 500 ) {
 					BufferedReader br = null;
 					StringBuilder  sb = new StringBuilder();
@@ -316,8 +168,20 @@ public class StatusUpdater {
 						res.getStatusLine().getReasonPhrase() +
 						( responseBody == null ? "" : "\nレスポンスボディ : " + responseBody ) );
 			} else {
-				System.out.println( res.getStatusLine().getStatusCode() );
-				System.out.println( res.getEntity() );
+				Reader br = null;
+				StringBuilder sb = new StringBuilder();
+				try {
+					char[] buf = new char[4096];
+					br = new InputStreamReader( res.getEntity().getContent() );
+					while( true ) {
+						int len = br.read( buf );
+						if( len < 0 ) break;
+						sb.append( buf, 0, len );
+					}
+				} finally {
+					br.close();
+				}
+				responseBody = sb.toString();
 			}
 			HttpEntity ent = res.getEntity();
 			is = ent.getContent();
@@ -338,6 +202,7 @@ public class StatusUpdater {
 			//if( conn != null ) conn.disconnect();
             if( httpclient != null ) httpclient.getConnectionManager().shutdown();
 		}
+		return responseBody;
 	}
 	
 }
